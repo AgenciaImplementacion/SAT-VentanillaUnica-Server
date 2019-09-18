ae25d6ac6cee67bfc0bc6d78017adc850fcff5bd
Jhon Freddy Rondon <jhonfr29@gmail.com>
2019-09-18 13:51:32 -0500

    ajustes consultas

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,7 @@
-INSERT INTO vu.restrictions VALUES (1, 'Restricción 1');
-INSERT INTO vu.restrictions VALUES (2, 'Restricción 2');
-INSERT INTO vu.restrictions VALUES (3, 'Restricción 3');
-INSERT INTO vu.restrictions VALUES (4, 'Restricción 4');
+INSERT INTO vu.restrictions VALUES (1, 'Uso');
+INSERT INTO vu.restrictions VALUES (2, 'Disposición');
+INSERT INTO vu.restrictions VALUES (3, 'Dominio');
+INSERT INTO vu.restrictions VALUES (4, 'Ocupación');
 
 INSERT INTO vu.organization VALUES (1, 'Departamento Nacional de Planeación');
 INSERT INTO vu.organization VALUES (2, 'Parques Nacionales Naturales de Colombia');
